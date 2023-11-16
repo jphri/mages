6a08637fdf396468e3eef22267e60b3d1f6231e5
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2023-11-16 18:56:57 -0300

    FASTFIX: restore editor directory

---
+++ src/editor/select_tiles.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+#include "../global.h"
+#include "../vecmath.h"
+#include "../physics.h"
+#include "../entity.h"
+#include "../graphics.h"
+#include "../util.h"
+#include "../map.h"
+#include "editor.h"
+
+static vec2 edit_offset = {0}, edit_move_offset = {0}, edit_begin_offset = {0};
+static float edit_zoom = 16;
+static bool ctrl_pressed = false;
+
+static MouseState edit_mouse_state;
+
+void
+select_tile_render()
+{
+	gfx_set_camera(edit_offset, (vec2){ edit_zoom, edit_zoom });
+
+	gfx_draw_begin(NULL);
+	for(int i = 0; i < 16 * 16; i++) {
+		float x = (     (i % editor.map->w) + 0.5);
+		float y = ((int)(i / editor.map->w) + 0.5);
+
+		int spr = i - 1;
+		int spr_x = spr % 16;
+		int spr_y = spr / 16;
+
+		gfx_draw_sprite(&(Sprite){
+				.position = { x, y },
+				.sprite_id = { spr_x, spr_y },
+				.color = { 1.0, 1.0, 1.0, 1.0 },
+				.half_size = { 0.5, 0.5 },
+				.type = editor.map_atlas,
+				.clip_region = { 0, 0, 1000, 1000 }
+				});
+	}
+	for(int x = 0; x < 17; x++) {
+		gfx_draw_line(
+			TEXTURE_UI,
+			(vec2){ (x - 0.1), (0.0) }, 
+			(vec2){ (x - 0.1), (editor.map->h) },
+			0.1,
+			(vec4){ 1.0, 1.0, 1.0, 1.0 },
+			(vec4){ 0.0, 0.0, 1000, 1000 }
+		);
+	}
+	for(int y = 0; y < editor.map->h + 1; y++) {
+		gfx_draw_line(
+			TEXTURE_UI,
+			(vec2){ (0.0),           (y - 0.1) }, 
+			(vec2){ (editor.map->w), (y - 0.1) },
+			0.1,
+			(vec4){ 1.0, 1.0, 1.0, 1.0 },
+			(vec4){ 0.0, 0.0, 1000, 1000 }
+		);
+	}
+	gfx_draw_end();
+}
+
+void
+select_tile_keyboard(SDL_Event *event)
+{
+	if(event->type == SDL_KEYDOWN) {
+		switch(event->key.keysym.sym) {
+		case SDLK_1: editor.editor_state = EDITOR_EDIT_MAP; break;
+		case SDLK_2: editor.editor_state = EDITOR_SELECT_TILE; break;
+		case SDLK_3: editor.editor_state = EDITOR_EDIT_COLLISION; break;
+		case SDLK_LCTRL: ctrl_pressed = true; break;
+		case SDLK_s:
+			if(ctrl_pressed) {
+				export_map("exported_map.map");
+				printf("Map exported to exported_map.map\n");
+			}
+			break;
+		}
+	} else {
+		switch(event->key.keysym.sym) {
+		case SDLK_LCTRL: ctrl_pressed = false; break;
+		}
+	}
+}
+
+void
+select_tile_wheel(SDL_Event *event)
+{
+	edit_zoom += event->wheel.y;
+	if(edit_zoom < 1.0)
+		edit_zoom = 1.0;
+}
+
+void
+select_tile_mouse_motion(SDL_Event *event)
+{
+	int x, y;
+	if(editor.editor_state == EDITOR_SELECT_TILE) {
+		switch(edit_mouse_state) {
+		case MOUSE_MOVING:
+			edit_offset[0] = edit_begin_offset[0] + event->motion.x - edit_move_offset[0];
+			edit_offset[1] = edit_begin_offset[1] + event->motion.y - edit_move_offset[1];
+			break;
+		case MOUSE_DRAWING:
+			x = ((event->motion.x - edit_offset[0]) / edit_zoom);
+			y = ((event->motion.y - edit_offset[1]) / edit_zoom);
+			if(x < 0 || x >= 16 || y < 0 || y >= 16) return;
+			editor.current_tile = x + y * 16;
+		default:
+			do {} while(0);
+		}
+	}
+}
+
+void
+select_tile_mouse_button(SDL_Event *event)
+{
+	int x, y;
+	if(event->type == SDL_MOUSEBUTTONUP) {
+		edit_mouse_state = MOUSE_NOTHING;
+	}
+	if(event->type == SDL_MOUSEBUTTONDOWN) {
+		switch(event->button.button) {
+		case SDL_BUTTON_LEFT:
+			x = ((event->motion.x - edit_offset[0]) / edit_zoom);
+			y = ((event->motion.y - edit_offset[1]) / edit_zoom);
+			if(x < 0 || x >= editor.map->w || y < 0 || y >= editor.map->h) return;
+			editor.current_tile = x + y * 16;
+			printf("Tile Selected: %d\n", editor.current_tile);
+			break;
+		case SDL_BUTTON_RIGHT: 
+			edit_move_offset[0] = event->button.x; 
+			edit_move_offset[1] = event->button.y; 
+			vec2_dup(edit_begin_offset, edit_offset);
+			edit_mouse_state = MOUSE_MOVING; 
+			break;
+		}
+	}
+}
---
+++ src/editor/tile_map_editor.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <glad/gles2.h>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+#include "../global.h"
+#include "../vecmath.h"
+#include "../physics.h"
+#include "../entity.h"
+#include "../graphics.h"
+#include "../util.h"
+#include "../map.h"
+#include "editor.h"
+
+Global GLOBAL;
+EditorGlobal editor;
+
+static GLADapiproc load_proc(const char *name) 
+{
+	GLADapiproc proc;
+
+	*(void**)(&proc)= SDL_GL_GetProcAddress(name);
+	return proc;
+}
+
+static State state_vtable[] = {
+	[EDITOR_EDIT_MAP] = {
+		.render = edit_render,
+		.wheel = edit_wheel,
+		.keyboard = edit_keyboard,
+		.mouse_button = edit_mouse_button,
+		.mouse_motion = edit_mouse_motion
+	},
+	[EDITOR_SELECT_TILE] = {
+		.render       = select_tile_render,
+		.wheel        = select_tile_wheel,
+		.keyboard     = select_tile_keyboard,
+		.mouse_button = select_tile_mouse_button,
+		.mouse_motion = select_tile_mouse_motion
+	},
+	[EDITOR_EDIT_COLLISION] = {
+		.render       = collision_render,
+		.wheel        = collision_wheel,
+		.keyboard     = collision_keyboard,
+		.mouse_button = collision_mouse_button,
+		.mouse_motion = collision_mouse_motion
+	}
+};
+
+int
+main(int argc, char *argv[])
+{
+	if(argc == 3) {
+		editor.map = map_alloc(atoi(argv[1]), atoi(argv[2]));
+	}
+	if(argc == 2) {
+		load_map(argv[1]);
+	}
+
+	editor.map_atlas = SPRITE_TERRAIN;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+		return -1;
+
+	GLOBAL.window = SDL_CreateWindow("hello",
+							  SDL_WINDOWPOS_UNDEFINED,
+							  SDL_WINDOWPOS_UNDEFINED,
+							  800, 600,
+							  SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	GLOBAL.renderer = SDL_CreateRenderer(GLOBAL.window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	GLOBAL.glctx = SDL_GL_CreateContext(GLOBAL.window);
+	SDL_GL_MakeCurrent(GLOBAL.window, GLOBAL.glctx);
+	gladLoadGLES2(load_proc);
+
+	gfx_init();
+	while(true) {
+		int w, h;
+		SDL_Event event;
+		SDL_GetWindowSize(GLOBAL.window, &w, &h);
+
+		gfx_make_framebuffers(w, h);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		if(state_vtable[editor.editor_state].render)
+			state_vtable[editor.editor_state].render();
+		
+		SDL_GL_SwapWindow(GLOBAL.window);
+		while(SDL_PollEvent(&event)) {
+			switch(event.type) {
+			case SDL_QUIT:
+				goto end_loop;
+			case SDL_MOUSEBUTTONUP:
+			case SDL_MOUSEBUTTONDOWN:
+				if(state_vtable[editor.editor_state].mouse_button)
+					state_vtable[editor.editor_state].mouse_button(&event);
+				break;
+			case SDL_MOUSEWHEEL:
+				if(state_vtable[editor.editor_state].wheel)
+					state_vtable[editor.editor_state].wheel(&event);
+				break;
+			case SDL_MOUSEMOTION:
+				if(state_vtable[editor.editor_state].mouse_motion)
+					state_vtable[editor.editor_state].mouse_motion(&event);
+				break;
+			case SDL_KEYDOWN:
+				if(state_vtable[editor.editor_state].keyboard)
+					state_vtable[editor.editor_state].keyboard(&event);
+				break;
+			case SDL_KEYUP:
+				if(state_vtable[editor.editor_state].keyboard)
+					state_vtable[editor.editor_state].keyboard(&event);
+			}
+		}
+	}
+
+end_loop:
+	gfx_end();
+
+	SDL_DestroyRenderer(GLOBAL.renderer);
+	SDL_DestroyWindow(GLOBAL.window);
+	SDL_Quit();
+	return 0;
+}
+
+void
+export_map(const char *map_file) 
+{
+	size_t map_data_size;
+	char *map_data = map_export(editor.map, &map_data_size);
+	FILE *fp = fopen(map_file, "w");
+	if(!fp)
+		goto error_open;
+	
+	int f = fwrite(map_data, 1, map_data_size, fp);
+	if(f < (int)(map_data_size))
+		printf("Error saving file: %s\n", map_file);
+	else
+		printf("File saved at %s\n", map_file);
+	
+	fclose(fp);
+	
+error_open:
+	free(map_data);
+}
+
+void
+load_map(const char *map_file)
+{
+	editor.map = map_load(map_file);
+	if(!editor.map)
+		die("failed loading file %s\n", map_file);
+}
+
