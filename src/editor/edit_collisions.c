19e04e5c03c876a049ff125ded3f04b1c7f8bc56
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2024-02-19 23:09:44 -0300

    collision: added cursor abstraction

--- src/editor/edit_collisions.c
+++ src/editor/edit_collisions.c
@@ -13,7 +13,38 @@
 
 #define MAX_LAYERS 64
 
+typedef enum CursorMode {
+	CURSOR_RECTANGLE,
+	LAST_CURSOR_MODE
+} CursorMode;
+
+typedef struct {
+	void (*begin)(int x, int y);
+	void (*drag)(int x, int y);
+	void (*end)(int x, int y);
+	void (*draw)(void);
+} Cursor;
+
+static void cursor_begin(int x, int y);
+static void cursor_drag(int x, int y);
+static void cursor_end(int x, int y);
+static void cursor_draw(void);
+
 static void layer_slider_cbk(UIObject obj, void *userptr);
+
+static void rect_begin(int x, int y);
+static void rect_drag(int x, int y);
+static void rect_end(int x, int y);
+static void rect_draw(void);
+
+static Cursor cursor[] = {
+	[CURSOR_RECTANGLE] = {
+		.begin = rect_begin,
+		.drag = rect_drag,
+		.end = rect_end,
+		.draw = rect_draw
+	}
+};
 
 static float zoom = 16.0;
 static bool  ctrl_pressed = false;
@@ -21,6 +52,7 @@ static vec2  begin_offset, move_offset, offset;
 static int current_layer = 0;
 static MouseState mouse_state;
 static CollisionData current_collision;
+static CursorMode current_cursor;
 
 static UIObject general_root;
 static UIObject after_layer_alpha_slider;
@@ -36,7 +68,7 @@ collision_init(void)
 		{
 			UIObject sublayout, label;
 
-#define BEGIN_SUB(LABEL_STRING) \
+			#define BEGIN_SUB(LABEL_STRING) \
 			sublayout = ui_layout_new(); \
 			ui_layout_set_order(sublayout, UI_LAYOUT_HORIZONTAL); \
 			ui_layout_set_border(sublayout, 2.5, 2.5, 2.5, 2.5); \
@@ -46,11 +78,9 @@ collision_init(void)
 			ui_label_set_color(label, (vec4){ 1.0, 1.0, 1.0, 1.0 }); \
 			ui_layout_append(sublayout, label); \
 
-#define END_SUB ui_layout_append(general_layout, sublayout)
+			#define END_SUB ui_layout_append(general_layout, sublayout)
 
 			BEGIN_SUB("Layer: ") {
-
-
 				UIObject slider_size = ui_slider_new();
 				ui_slider_set_min_value(slider_size, 0.0);
 				ui_slider_set_max_value(slider_size, MAX_LAYERS - 1);
@@ -115,33 +145,13 @@ collision_keyboard(SDL_Event *event)
 void
 collision_mouse_motion(SDL_Event *event)
 {
-	vec2 full_size, begin_position;
-	vec2 v;
-
 	switch(mouse_state) {
 	case MOUSE_MOVING:
 		offset[0] = begin_offset[0] + event->motion.x - move_offset[0];
 		offset[1] = begin_offset[1] + event->motion.y - move_offset[1];
 		break;
 	case MOUSE_DRAWING:
-		gfx_pixel_to_world((vec2){ event->motion.x, event->motion.y }, v);
-
-		vec2_sub(full_size, v, begin_offset);
-		vec2_div(current_collision.half_size, full_size, (vec2){ 2.0, 2.0 });
-		current_collision.half_size[0] = fabs(current_collision.half_size[0]);
-		current_collision.half_size[1] = fabs(current_collision.half_size[1]);
-		
-		if(full_size[0] > 0)
-			begin_position[0] = begin_offset[0];
-		else
-			begin_position[0] = begin_offset[0] + full_size[0];
-
-		if(full_size[1] > 0)
-			begin_position[1] = begin_offset[1];
-		else
-			begin_position[1] = begin_offset[1] + full_size[1];
-		
-		vec2_add(current_collision.position, begin_position, current_collision.half_size);
+		cursor_drag(event->motion.x, event->motion.y);
 		break;
 	default:
 		do {} while(0);
@@ -154,16 +164,7 @@ collision_mouse_button(SDL_Event *event)
 	vec2 pos;
 	if(event->type == SDL_MOUSEBUTTONUP) {
 		if(mouse_state == MOUSE_DRAWING) {
-			CollisionData *data = malloc(sizeof(*data));
-			*data = current_collision;
-			data->next = editor.map->collision;
-			editor.map->collision = data;
-
-			printf("new collision at %f %f %f %f\n",
-					data->position[0],
-					data->position[1],
-					data->half_size[0],
-					data->half_size[1]);
+			cursor_end(event->button.x, event->button.y);
 		}
 		mouse_state = MOUSE_NOTHING;
 	}
@@ -171,7 +172,7 @@ collision_mouse_button(SDL_Event *event)
 	if(event->type == SDL_MOUSEBUTTONDOWN && mouse_state == MOUSE_NOTHING) {
 		switch(event->button.button) {
 		case SDL_BUTTON_LEFT: 
-			gfx_pixel_to_world((vec2){ event->button.x, event->button.y }, begin_offset);
+			cursor_begin(event->button.x, event->button.y);
 			mouse_state = MOUSE_DRAWING; 
 			break;
 		case SDL_BUTTON_RIGHT: 
@@ -262,9 +263,37 @@ collision_render(void)
 	}
 
 	if(mouse_state == MOUSE_DRAWING) {
-		gfx_draw_rect(current_collision.position, current_collision.half_size, 0.15, (vec4){ 1.0, 1.0, 1.0, 1.0 });
+		cursor_draw();
 	}
 	gfx_draw_end();
+}
+
+void
+cursor_begin(int x, int y)
+{
+	if(cursor[current_cursor].begin)
+		cursor[current_cursor].begin(x, y);
+}
+
+void
+cursor_drag(int x, int y)
+{
+	if(cursor[current_cursor].drag)
+		cursor[current_cursor].drag(x, y);
+}
+
+void
+cursor_end(int x, int y)
+{
+	if(cursor[current_cursor].end)
+		cursor[current_cursor].end(x, y);
+}
+
+void
+cursor_draw(void)
+{
+	if(cursor[current_cursor].draw)
+		cursor[current_cursor].draw();
 }
 
 void
@@ -272,4 +301,60 @@ layer_slider_cbk(UIObject slider, void *userptr)
 {
 	(void)userptr;
 	current_layer = ui_slider_get_value(slider);
+}
+
+void
+rect_begin(int x, int y)
+{
+	gfx_pixel_to_world((vec2){ x, y }, begin_offset);
+}
+
+void
+rect_drag(int x, int y)
+{
+	vec2 full_size, begin_position;
+	vec2 v;
+
+	gfx_pixel_to_world((vec2){ x, y }, v);
+
+	vec2_sub(full_size, v, begin_offset);
+	vec2_div(current_collision.half_size, full_size, (vec2){ 2.0, 2.0 });
+	current_collision.half_size[0] = fabs(current_collision.half_size[0]);
+	current_collision.half_size[1] = fabs(current_collision.half_size[1]);
+
+	if(full_size[0] > 0)
+		begin_position[0] = begin_offset[0];
+	else
+		begin_position[0] = begin_offset[0] + full_size[0];
+
+	if(full_size[1] > 0)
+		begin_position[1] = begin_offset[1];
+	else
+		begin_position[1] = begin_offset[1] + full_size[1];
+
+	vec2_add(current_collision.position, begin_position, current_collision.half_size);
+}
+
+void
+rect_end(int x, int y)
+{
+	(void)x;
+	(void)y;
+
+	CollisionData *data = malloc(sizeof(*data));
+	*data = current_collision;
+	data->next = editor.map->collision;
+	editor.map->collision = data;
+
+	printf("new collision at %f %f %f %f\n",
+			data->position[0],
+			data->position[1],
+			data->half_size[0],
+			data->half_size[1]);
+}
+
+void
+rect_draw(void)
+{
+	gfx_draw_rect(current_collision.position, current_collision.half_size, 0.15, (vec4){ 1.0, 1.0, 1.0, 1.0 });
 }
